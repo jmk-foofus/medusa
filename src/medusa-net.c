9c704524f019e921e7c96a9044c1359ea67dcd7b
jmk-foofus <jmk@foofus.net>
2015-11-05 16:09:01 -0600

    Fix for OpenSSL that has been built without SSLv3.

--- src/medusa-net.c
+++ src/medusa-net.c
@@ -352,12 +352,18 @@ int medusaConnectSSLInternal(sConnectParams* pParams, int hSocket)
     sslContext = SSL_CTX_new(SSLv2_client_method());
   else
 #endif
+#ifndef OPENSSL_NO_SSL3
   if (pParams->nSSLVersion == 3)
     sslContext = SSL_CTX_new(SSLv3_client_method());
-  else if (pParams->nSSLVersion == (float)3.1)
+  else
+#endif
+  if (pParams->nSSLVersion == (float)3.1)
     sslContext = SSL_CTX_new(TLSv1_client_method());
   else
+  {
     sslContext = SSL_CTX_new(SSLv23_client_method());
+    //SSL_CTX_set_options(sslContext, SSL_OP_NO_SSLv3 | SSL_OP_NO_SSLv2);
+  }
 
   if (sslContext == NULL)
   {
