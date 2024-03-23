2ee0e9ae9a82dd7a714e9e55c1d485895e59b55d
jmk-foofus <jmk@foofus.net>
2024-03-23 12:34:43 -0400

    Refactor SMBv2 support and merge into SMBNT module.

---
+++ src/modsrc/smbnt-smb2.c
@@ -0,0 +1,220 @@
+#include "smbnt.h"
+
+#ifdef BUILD_MODULE_SMBNT_SMB2
+
+#define SMBv2 16
+
+/* libsmb2:include/libsmb2-private.h */
+#ifndef SMB2_SEC_NTLMSSP
+#define SMB2_SEC_UNDEFINED 0 /* use KRB if available, otherwise NTLM */
+#define SMB2_SEC_NTLMSSP 1
+#define SMB2_SEC_KRB5 2
+#endif
+
+/*
+  https://docs.microsoft.com/en-us/windows-server/storage/file-server/troubleshoot/smbv1-not-installed-by-default-in-windows
+  * 2020/07/01
+  * New installations of Windows 10 and Server 2016 or later no longer support SMBv1.
+*/
+int SMB2NegProt(int hSocket, _SMBNT_DATA* _psSessionData)
+{
+  /* Dialect: SMB 2.??? (highest) */
+  unsigned char buf[179] = {
+    0x00, 0x00, 0x00, 0xaf, 0xff, 0x53, 0x4d, 0x42,
+    0x72, 0x00, 0x00, 0x00, 0x00, 0x08, 0x01, 0x40,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3c, 0x7d,
+    0x00, 0x00, 0x01, 0x00, 0x00, 0x8b, 0x00, 0x02,
+    0x50, 0x43, 0x20, 0x4e, 0x45, 0x54, 0x57, 0x4f,
+    0x52, 0x4b, 0x20, 0x50, 0x52, 0x4f, 0x47, 0x52,
+    0x41, 0x4d, 0x20, 0x31, 0x2e, 0x30, 0x00, 0x02,
+    0x4d, 0x49, 0x43, 0x52, 0x4f, 0x53, 0x4f, 0x46,
+    0x54, 0x20, 0x4e, 0x45, 0x54, 0x57, 0x4f, 0x52,
+    0x4b, 0x53, 0x20, 0x31, 0x2e, 0x30, 0x33, 0x00,
+    0x02, 0x4d, 0x49, 0x43, 0x52, 0x4f, 0x53, 0x4f,
+    0x46, 0x54, 0x20, 0x4e, 0x45, 0x54, 0x57, 0x4f,
+    0x52, 0x4b, 0x53, 0x20, 0x33, 0x2e, 0x30, 0x00,
+    0x02, 0x4c, 0x41, 0x4e, 0x4d, 0x41, 0x4e, 0x31,
+    0x2e, 0x30, 0x00, 0x02, 0x4c, 0x4d, 0x31, 0x2e,
+    0x32, 0x58, 0x30, 0x30, 0x32, 0x00, 0x02, 0x53,
+    0x61, 0x6d, 0x62, 0x61, 0x00, 0x02, 0x4e, 0x54,
+    0x20, 0x4c, 0x41, 0x4e, 0x4d, 0x41, 0x4e, 0x20,
+    0x31, 0x2e, 0x30, 0x00, 0x02, 0x4e, 0x54, 0x20,
+    0x4c, 0x4d, 0x20, 0x30, 0x2e, 0x31, 0x32, 0x00,
+    0x02, 0x53, 0x4d, 0x42, 0x20, 0x32, 0x2e, 0x3f,
+    0x3f, 0x3f, 0x00
+  };
+
+  unsigned char* bufReceive;
+  int nReceiveBufferSize = 0;
+
+  if (medusaSend(hSocket, buf, sizeof(buf), 0) < 0)
+  {
+    writeError(ERR_ERROR, "%s failed: medusaSend was not successful", MODULE_NAME);
+  }
+
+  nReceiveBufferSize = 0;
+  bufReceive = medusaReceiveRaw(hSocket, &nReceiveBufferSize);
+  if (bufReceive == NULL)
+    return FAILURE;
+
+  /* SMBv2 negotiation (SMBv1: 0xff) */
+  if (bufReceive[4] == 0xfe) {
+    writeVerbose(VB_GENERAL, "%s: Server negotiated SMBv2", MODULE_NAME);
+    _psSessionData->smbVersion = SMBv2;
+
+    _psSessionData->smb2 = smb2_init_context();
+    if (_psSessionData->smb2 == NULL) {
+      writeError(ERR_ERROR, "[%s] Failed to initialize context.", MODULE_NAME);
+      return FAILURE;
+    }
+
+    smb2_set_authentication(_psSessionData->smb2, SMB2_SEC_NTLMSSP);
+    smb2_set_security_mode(_psSessionData->smb2, SMB2_NEGOTIATE_SIGNING_ENABLED);
+
+    return SUCCESS;
+  }
+
+  return FAILURE;
+}
+
+/*
+  SMB2ConvertPassword
+  Function: Prepare NTLM password hash for libsmb2.
+*/
+int SMB2ConvertPassword(_SMBNT_DATA *_psSessionData, unsigned char* szPassword, unsigned char** szPassword2)
+{
+  unsigned int i = 0;
+  unsigned char *p = NULL;
+  unsigned char NO_PASSWORD[1] = "";
+
+  /* Use NTLM Hash instead of password */
+  /* D42E35E1A1E4C22BD32E2170E4857C20:5E20780DD45857A68402938C7629D3B2::: */
+  if (_psSessionData->hashFlag == HASH) {
+    p = szPassword;
+    while ((*p != '\0') && (i < 1)) {
+      if (*p == ':')
+        i++;
+      p++;
+    }
+  }
+
+  /* If "-e ns" was used, don't treat these values as hashes. */
+  if ((_psSessionData->hashFlag == HASH) && (i >= 1)) {
+    if (*p == '\0') {
+      writeError(ERR_ERROR, "Error reading PwDump file.");
+      return FAILURE;
+    }
+    else if (*p == 'N') {
+      writeError(ERR_DEBUG_MODULE, "Found \"NO PASSWORD\" for NTLM Hash.");
+      *szPassword2 = NO_PASSWORD;
+    }
+    else {
+      memset(p + 32, '\0', 1);
+      writeError(ERR_DEBUG_MODULE, "Prepare ASCII PwDump NTLM Hash (%s).", p);
+      if (asprintf((char **)szPassword2, "ntlm:%s", p) < 0) { return FAILURE; }
+    }
+  } else {
+    *szPassword2 = szPassword;
+    writeError(ERR_DEBUG_MODULE, "[%s] Using standard password: %s", MODULE_NAME, *szPassword2);
+  }
+
+  return SUCCESS;
+}
+
+/*
+  SMB2SessionSetup
+*/
+unsigned long SMB2SessionSetup(int hSocket, sLogin** psLogin, _SMBNT_DATA *_psSessionData, char* szLogin, char* szPassword)
+{
+  int SMBerr, SMBaction;
+  unsigned long SMBSessionRet;
+  const char *pErrorMsg = NULL;
+  char ErrorCode[10];
+  int iRet;
+  unsigned int i;
+  unsigned char* szPassword2 = NULL;
+
+  regex_t preg;
+  int errcode = REG_NOMATCH;
+  char errmsg[512];
+  size_t nmatch = 1;
+  regmatch_t pmatch[1];
+
+  if (_psSessionData->accntFlag == LOCAL) {
+    strcpy((char *) _psSessionData->workgroup, ".");
+  } else if (_psSessionData->accntFlag == BOTH) {
+    memset(_psSessionData->workgroup, 0, 16);
+  } else if (_psSessionData->accntFlag == OTHER) {
+    strncpy(_psSessionData->workgroup, _psSessionData->workgroup_other, 16);
+  }
+
+  writeError(ERR_DEBUG_MODULE, "[%s] Set authentication request data.", MODULE_NAME);
+  smb2_set_domain(_psSessionData->smb2, _psSessionData->workgroup);
+  smb2_set_user(_psSessionData->smb2, szLogin);
+
+  if (SMB2ConvertPassword(_psSessionData, szPassword, &szPassword2) == FAILURE) {
+    writeError(ERR_ERROR, "Failed to prepare libsmb2 password.");
+    return FAILURE;
+  }
+
+  smb2_set_password(_psSessionData->smb2, szPassword2);
+
+  writeError(ERR_DEBUG_MODULE, "[%s] Initiate SMB2 connection.", MODULE_NAME);
+  if (smb2_connect_share(_psSessionData->smb2, (*psLogin)->psServer->pHostIP, "ADMIN$", NULL) < 0) {
+
+    pErrorMsg = smb2_get_error(_psSessionData->smb2);
+    writeError(ERR_DEBUG_MODULE, "[%s] Failed to connect to ADMIN$: %s", MODULE_NAME, pErrorMsg);
+
+    /* Extract error code from libsmb2 error message:
+       ERROR: [smb2.mod] Failed to connect to ADMIN$: Session setup failed with (0xc000006d) STATUS_LOGON_FAILURE
+    */
+    errcode = regcomp(&preg, "0x[a-fA-F0-9]+", REG_EXTENDED|REG_ICASE);
+    if (errcode)
+    {
+      memset(errmsg, 0, 512);
+      regerror(errcode, &preg, errmsg, 512);
+      writeError(ERR_ERROR, "Regex compilation failed: %s", errmsg);
+      return FAILURE;
+    }
+
+    errcode = regexec(&preg, pErrorMsg, nmatch, pmatch, 0);
+    if (errcode == REG_NOMATCH)
+    {
+      writeError(ERR_ERROR, "[%s] Regex failed to match smb2_connect_share error message.", MODULE_NAME);
+      return FAILURE;
+    }
+    else {
+      writeError(ERR_DEBUG_MODULE, "[%s] Regex successfully matched smb2_connect_share error message.", MODULE_NAME);
+
+      memset(errmsg, 0, 512);
+      memcpy(errmsg, pErrorMsg + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+      SMBSessionRet = (int)strtol(errmsg, NULL, 0);
+
+      writeError(ERR_DEBUG_MODULE, "[%s] smb2_connect_share session return code: 0x%6.6X", MODULE_NAME, SMBSessionRet);
+    }
+  }
+  else {
+    writeError(ERR_DEBUG_MODULE, "[%s] smb2_connect_share returned without error.", MODULE_NAME);
+    SMBSessionRet = 0x00000000;
+    smb2_disconnect_share(_psSessionData->smb2);
+  }
+
+  return SMBSessionRet;
+}
+
+#else
+
+int SMB2NegProt(int hSocket, _SMBNT_DATA* _psSessionData)
+{
+  writeVerbose(VB_NONE, "** Module was not built with LIBSMB2 support **");
+  return FAILURE;
+}
+
+unsigned long SMB2SessionSetup(int hSocket, sLogin** psLogin, _SMBNT_DATA *_psSessionData, char* szLogin, char* szPassword)
+{
+  writeVerbose(VB_NONE, "** Module was not built with LIBSMB2 support **");
+  return FAILURE;
+}
+
+#endif
---
+++ src/modsrc/smbnt.h
@@ -0,0 +1,128 @@
+/*
+**   SMB LAN Manager Password/HASH Checking Medusa Module
+**
+**   ------------------------------------------------------------------------
+**    Copyright (C) 2024 Joe Mondloch
+**    JoMo-Kun / jmk@foofus.net
+**
+**    This program is free software; you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License version 2,
+**    as published by the Free Software Foundation
+**
+**    This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    http://www.gnu.org/licenses/gpl.txt
+**
+**    This program is released under the GPL with the additional exemption
+**    that compiling, linking, and/or using OpenSSL is allowed.
+**
+*/
+
+#include <sys/types.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <regex.h>
+#include "module.h"
+
+#include <smb2/smb2.h>
+#include <smb2/libsmb2.h>
+//#include <smb2/libsmb2-raw.h>
+//#include <smb2/libsmb2-dcerpc-srvsvc.h>
+
+#define MODULE_NAME    "smbnt.mod"
+#define MODULE_AUTHOR  "JoMo-Kun <jmk@foofus.net>"
+#define MODULE_SUMMARY_USAGE  "Brute force module for SMBv[1-3] (LM/NTLM/LMv2/NTLMv2) sessions"
+#define MODULE_VERSION    "3.0"
+#define MODULE_VERSION_SVN "$Id: smbnt.c 9239 2015-05-22 15:03:03Z jmk $"
+#define MODULE_SUMMARY_FORMAT  "%s : version %s"
+#define MODULE_SUMMARY_FORMAT_WARN  "%s : version %s (%s)"
+#define OPENSSL_WARNING "No usable OPENSSL. Module disabled."
+
+#ifdef HAVE_LIBSSL
+#include <openssl/evp.h>
+#endif
+
+#include "hmacmd5.h"
+
+#define PORT_NBNS 137
+#define PORT_SMB 139
+#define PORT_SMBNT 445
+#define WIN2000_NATIVEMODE 1
+#define WIN_NETBIOSMODE 2
+#define PASSWORD 3
+#define HASH 4
+#define MACHINE_NAME 5
+#define LOCAL 6
+#define NTDOMAIN 7
+#define BOTH 8
+#define OTHER 9
+#define PLAINTEXT 10
+#define ENCRYPTED 11
+#define AUTH_LM 12
+#define AUTH_NTLM 13
+#define AUTH_LMv2 14
+#define AUTH_NTLMv2 15
+#define SMBv2 16
+
+#ifndef CHAR_BIT
+#define CHAR_BIT 8
+#endif
+
+#ifndef TIME_T_MIN
+#define TIME_T_MIN ((time_t)0 < (time_t) -1 ? (time_t) 0 \
+        : ~ (time_t) 0 << (sizeof (time_t) * CHAR_BIT - 1))
+#endif
+#ifndef TIME_T_MAX
+#define TIME_T_MAX (~ (time_t) 0 - TIME_T_MIN)
+#endif
+
+#define IVAL_NC(buf,pos) (*(unsigned int *)((char *)(buf) + (pos))) /* Non const version of above. */
+#define SIVAL(buf,pos,val) IVAL_NC(buf,pos)=((unsigned int)(val))
+
+#if (SIZEOF_LONG == 8)
+#define TIME_FIXUP_CONSTANT_INT 11644473600L
+#elif (SIZEOF_LONG_LONG == 8)
+#define TIME_FIXUP_CONSTANT_INT 11644473600LL
+#endif
+
+typedef struct __SMBNT_DATA {
+#ifdef HAVE_LIBSMB2
+  struct smb2_context *smb2;
+#endif
+  unsigned char challenge[8];
+  char workgroup[16];
+  char workgroup_other[16];
+  unsigned char machine_name[16];
+  int security_mode;
+  int authLevel;
+  int hashFlag;
+  int accntFlag;
+  int protoFlag;
+  int smbVersion;
+} _SMBNT_DATA;
+
+// Tells us whether we are to continue processing or not
+enum MODULE_STATE
+{
+  MSTATE_NEW,
+  MSTATE_RUNNING,
+  MSTATE_EXITING,
+  MSTATE_COMPLETE
+};
+
+int tryLogin(int hSocket, sLogin** login, _SMBNT_DATA* _psSessionData, char* szLogin, char* szPassword);
+int initModule(sLogin* login, _SMBNT_DATA *_psSessionData);
+
+char* parseFullyQualifiedUsername(_SMBNT_DATA *_psSessionData, char* szLogin);
+int NBSSessionRequest(int hSocket, _SMBNT_DATA* _psSessionData);
+int NBSTATQuery(sLogin *_psLogin,_SMBNT_DATA* _psSessionData);
+int SMBNegProt(int hSocket, _SMBNT_DATA* _psSessionData);
+unsigned long SMBSessionSetup(int hSocket, sLogin** psLogin, _SMBNT_DATA *_psSessionData, char* szLogin, char* szPassword);
+
+int SMB2NegProt(int hSocket, _SMBNT_DATA* _psSessionData);
+unsigned long SMB2SessionSetup(int hSocket, sLogin** psLogin, _SMBNT_DATA *_psSessionData, char* szLogin, char* szPassword);
